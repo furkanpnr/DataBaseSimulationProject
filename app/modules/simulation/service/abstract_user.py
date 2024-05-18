2f3f44e63f6f5b27cb04e2dfd68ad89f6062c6ec
furkaanpnr <furkanpnr220@gmail.com>
2024-05-18 15:41:27 +0300

    User has been restructured as thread. General update and optimization has been made.

--- app/modules/simulation/service/abstract_user.py
+++ app/modules/simulation/service/abstract_user.py
@@ -1,20 +1,45 @@
 from abc import ABC, abstractmethod
+from threading import Thread
 from modules.database.db_proxy import DBProxy
 
 class IUser(ABC):
-
     @abstractmethod
-    def start(self):
+    def simulate(self):
         pass
 
-class User(IUser):
+class User(IUser, Thread):
 
-    def __init__(self, db: DBProxy) -> None:
-        super().__init__()
+    def __init__(self, name: str, db: DBProxy, transaction_count: int) -> None:
+        """User for simulation
+
+        Args:
+            name (str): Name of the thraed user
+            db (DBProxy): Database proxy object
+            transaction_count (int): Number of transactions to simulate
+        """
+
+        super().__init__(name=name)
+        self.name = name
         self.db = db
+        self._transaction_count = transaction_count
+        self._elapsed_time = 0
 
-    def start(self):
-        raise NotImplementedError("Implement the method for User")
-    
+    def run(self):
+        raise NotImplementedError("Override the run method for user simulation")
+
     def simulate(self):
         raise NotImplementedError("Implement the method for user simulation")
+
+    @property
+    def elapsed_time(self) -> float:
+        return self._elapsed_time
+    
+    @elapsed_time.setter
+    def elapsed_time(self, value: float) -> None:
+        if not isinstance(value, (int, float)):
+            raise TypeError("Elapsed time must be a number")
+        self._elapsed_time = value
+
+    @property
+    def encountered_deadlock_num(self) -> int:
+        return self.db._encountered_deadlock_num
--- app/modules/simulation/service/simulation.py
+++ app/modules/simulation/service/simulation.py
@@ -1,51 +1,131 @@
-from config import DATABASE, SERVER, ISOLATION_LEVEL
-
-from modules.database.db_proxy import DBProxy
 from modules.simulation.service.users import AUser, BUser
+from modules.database.db_proxy import DBProxy
+from modules.database.enums import IsolationLevel
+from config import SERVER, DATABASE
 
-import datetime
-import threading
+import time
 import random
+
+from rich.console import Console
+from rich.table import Table
+
 
 class Simulation():
 
     @classmethod
-    def start(cls, a_user_num, b_user_num):
-        """Starts the simulation"""
-        start = datetime.datetime.now()
+    def start(cls, 
+              a_user_count: int, 
+              b_user_count: int,
+              transaction_count: int,
+              isolation_lvl: IsolationLevel):
+        
+        """Starts the transaction simulation on the database
 
-        users = cls.generate_users(a_user_num, b_user_num)
+        Args:
+            a_user_count (int): Number of A users
+            b_user_count (int): Number of B users
+            transaction_count (int): Number of transactions per user
+            isolation_lvl (str): Isolation level to be set for the transactions
+        """
+        users = cls.generate_users(a_user_count, b_user_count, transaction_count, isolation_lvl)
 
-        threads = []
+        
+        print("[bold green] Starting simulation \n")
+        begin_time = time.time()
+
         for user in users:
-            thread = threading.Thread(target=user.start)
-            threads.append(thread)
-            thread.start()
+            user.start()
+        
+        for user in users:
+            user.join()
+        
+        end_time = time.time()
+        print("[bold cyan] All users finished")
+        print(f"[bold]Simulation took {end_time - begin_time:.2f} seconds \n")
 
-        for thread in threads:
-            thread.join()
+        a_users = [user for user in users if isinstance(user, AUser)]
+        b_users = [user for user in users if isinstance(user, BUser)]
 
-        total_duration = datetime.datetime.now() - start
-        return total_duration
+        avg_a_user_time = sum([user.elapsed_time for user in a_users]) / len(a_users) if len(a_users) > 0 else 0
+        avg_b_user_time = sum([user.elapsed_time for user in b_users]) / len(b_users) if len(b_users) > 0 else 0
+
+        a_user_deadlocks = sum([user.encountered_deadlock_num for user in a_users])
+        b_user_deadlocks = sum([user.encountered_deadlock_num for user in b_users])
+
+        cls._print_result(a_users, b_users, avg_a_user_time, a_user_deadlocks, avg_b_user_time, b_user_deadlocks)
+
+        results = {
+            "a_user_count": len(a_users),
+            "b_user_count": len(b_users),
+            "avg_a_user_time": f"{avg_a_user_time:.2f} s",
+            "a_user_deadlocks": a_user_deadlocks,
+            "avg_b_user_time": f"{avg_b_user_time:.2f} s",
+            "b_user_deadlocks": b_user_deadlocks
+        }
+        return results
     
 
     @classmethod
-    def generate_users(cls, a_user_num, b_user_num):
-        """Generates the users"""
-
-        a_users = []
-        for _ in range(a_user_num):
-            db = DBProxy(SERVER, DATABASE, ISOLATION_LEVEL)
-            a_users.append(AUser(db))
-
-        b_users = []
-        for _ in range(b_user_num):
-            db = DBProxy(SERVER, DATABASE, ISOLATION_LEVEL)
-            b_users.append(BUser(db))
+    def generate_users(cls, 
+                       a_user_count: int, 
+                       b_user_count: int,
+                       transaction_count: int,
+                       isolation_lvl: IsolationLevel):
         
-        users = a_users + b_users
+        """Generates users
 
-        # return random shuffled user list
+        Args:
+            a_user_count (int): Number of A users
+            b_user_count (int): Number of B users
+            transaction_count (int): Number of transactions per user
+            isolation_lvl (IsolationLevel): Isolation level to be set for the transactions
+
+        Returns:
+            list: List of users to be simulated
+        """
+
+        users = []
+
+        for i in range(a_user_count):
+            db = DBProxy(SERVER, DATABASE, isolation_lvl)
+            users.append(AUser(name=f"AUser_{i}", db=db, transaction_count=transaction_count))
+        
+        for i in range(b_user_count):
+            db = DBProxy(SERVER, DATABASE, isolation_lvl)
+            users.append(BUser(name=f"BUser_{i}", db=db, transaction_count=transaction_count))
+        
         random.shuffle(users)
         return users
+    
+
+    @classmethod
+    def _print_result(cls, 
+                      a_users: list, 
+                      b_users: list, 
+                      avg_a_user_time: float, 
+                      a_user_deadlocks: int, 
+                      avg_b_user_time: float, 
+                      b_user_deadlocks: int):
+        
+        console = Console()
+
+        # Create a table
+        table = Table(title="Simulation Results")
+        table.add_column("Number of Type A Users", style="cyan")
+        table.add_column("Number of Type B Users", style="magenta")
+        table.add_column("Average Duration of Type A Threads", style="cyan")
+        table.add_column("Number of Deadlocks Encountered by Type A Users", style="magenta")
+        table.add_column("Average Duration of Type B Threads", style="cyan")
+        table.add_column("Number of Deadlocks Encountered by Type B Users", style="magenta")
+
+        table.add_row(
+            str(len(a_users)),
+            str(len(b_users)),
+            f"{avg_a_user_time:.2f} s",
+            str(a_user_deadlocks),
+            f"{avg_b_user_time:.2f} s",
+            str(b_user_deadlocks)
+        )
+
+        console.print(table)
     
